a869368bb3e0469e039801ef777b0cbfc0f0cec9
axl <rafael.deassis@yahoo.com.br>
2026-08-21 20:07:45 -0300

    calculadora

--- expo-02/app/index.tsx
+++ expo-02/app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default function Index() {
+export default function App() {
+  const [idade, setIdade] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  function calcular() {
+    let ano = 2026 - Number(idade);
+    setResultado('Você nasceu no ano de ' + ano);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ padding: 30, marginTop: 100 }}>
+      <Text>Digite sua idade:</Text>
+
+      <TextInput
+        placeholder="Sua idade"
+        keyboardType="numeric"
+        value={idade}
+        onChangeText={setIdade}
+        style={{ borderWidth: 1, padding: 10, marginTop: 10 }}
+      />
+
+      <Button title="Calcular" onPress={calcular} />
+
+      <Text style={{ marginTop: 20, fontSize: 20 }}>
+        {resultado}
+      </Text>
     </View>
   );
 }
