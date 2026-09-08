c5acfd18b6602e6cd8881a78f9a5b1dcbad471c0
axl <rafael.deassis@yahoo.com.br>
2026-09-08 19:56:54 -0300

    expo 57 && api

---
+++ expo-02/api/users.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://parseapi.back4app.com",
+  headers: {
+    "X-Parse-Application-Id": "F9PL8Gv7DCyZflxRptJuVBau0Gs5rjNTwemrdoOk",
+    "X-Parse-REST-API-Key": "Besw4IFlIg560POQEfbSyx5uut8rqi4S8WTRTSXx",
+  },
+});
+const headerJson = { "Content-Type": "application/json" };
+const headerSession = { ...headerJson, "X-Parse-Revocable-Session": "1" };
+const userURL = "/users";
+
+// 'user = { "password":"", "username": "","email": "" }'
+export async function userSignUp(user) {
+  const response = await instance.post(userURL, user, {
+    headers: headerSession,
+  });
+  return response.data;
+}
+
+export async function userLogin(user) {
+  const response = await instance.post("/login", user, {
+    headers: headerSession,
+  });
+  return response.data;
+}
+export async function userLogout(sessionToken) {
+  const response = await instance.post("/logout", {
+    headers: { ...headerSession, "X-Parse-Session-Token": sessionToken },
+  });
+  return response.data;
+} 
+
+export async function currentUser(sessionToken) {
+  const response = await instance.get(`${userURL}/me`, {
+    headers: { ...headerSession, "X-Parse-Session-Token": sessionToken },
+  });
+  return response.data;
+}
+
+export async function verificationEmailRequest(email) {
+  const response = await instance.post(
+    "/verificationEmailRequest",
+    { email },
+    {
+      headers: headerJson,
+    },
+  );
+  return response.data;
+}
+export async function requestPasswordReset(email) {
+  const response = await instance.post(
+    "/requestPasswordReset",
+    { email },
+    {
+      headers: headerJson,
+    },
+  );
+  return response.data;
+}
--- expo-02/app/index.tsx
+++ expo-02/app/index.tsx
@@ -1,153 +1,17 @@
-import React, { useState } from 'react';
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  Keyboard,
-  TouchableWithoutFeedback,
-  StyleSheet,
-} from 'react-native';
-
-export default function App() {
-  const [idade, setIdade] = useState('');
-  const [dia, setDia] = useState('');
-  const [mes, setMes] = useState('');
-  const [resultado, setResultado] = useState('');
-
-  function calcular() {
-    // Fecha o teclado
-    Keyboard.dismiss();
-
-    // Pega o ano atual automaticamente
-    const anoAtual = new Date().getFullYear();
-
-    // Converte os textos para números
-    const idadeNumero = Number(idade);
-    const diaNumero = Number(dia);
-    const mesNumero = Number(mes);
-
-    // Validação
-    if (
-      !idade ||
-      !dia ||
-      !mes ||
-      idadeNumero < 0 ||
-      diaNumero < 1 ||
-      diaNumero > 31 ||
-      mesNumero < 1 ||
-      mesNumero > 12
-    ) {
-      setResultado('Por favor, digite uma data e idade válidas.');
-      return;
-    }
-
-    const anoNascimento = anoAtual - idadeNumero;
-
-    setResultado(
-      `Você tem ${idadeNumero} anos e nasceu no dia ${diaNumero}/${mesNumero}/${anoNascimento}.`
-    );
-  }
-
+export default function Index() {
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-
-        <Text style={styles.titulo}>
-          Calculadora de Ano de Nascimento
-        </Text>
-
-        <Text style={styles.label}>
-          Qual é a sua idade?
-        </Text>
-
-        <TextInput
-          placeholder="Digite sua idade"
-          keyboardType="numeric"
-          value={idade}
-          onChangeText={setIdade}
-          returnKeyType="done"
-          onSubmitEditing={Keyboard.dismiss}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>
-          Qual é o dia do seu nascimento?
-        </Text>
-
-        <TextInput
-          placeholder="Digite o dia"
-          keyboardType="numeric"
-          value={dia}
-          onChangeText={setDia}
-          returnKeyType="done"
-          onSubmitEditing={Keyboard.dismiss}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>
-          Qual é o mês do seu nascimento?
-        </Text>
-
-        <TextInput
-          placeholder="Digite o mês"
-          keyboardType="numeric"
-          value={mes}
-          onChangeText={setMes}
-          returnKeyType="done"
-          onSubmitEditing={Keyboard.dismiss}
-          style={styles.input}
-        />
-
-        <View style={styles.botao}>
-          <Button
-            title="Calcular"
-            onPress={calcular}
-          />
-        </View>
-
-        <Text style={styles.resultado}>
-          {resultado}
-        </Text>
-
-      </View>
-    </TouchableWithoutFeedback>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Programação para Dispositivos Móveis</Text>
+      <Link href="/listaTarefa">Tarefas</Link>
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    paddingTop: 100,
-  },
-
-  titulo: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-
-  label: {
-    marginTop: 20,
-    fontSize: 16,
-  },
-
-  input: {
-    borderWidth: 1,
-    borderColor: '#999',
-    borderRadius: 8,
-    padding: 10,
-    marginTop: 10,
-    fontSize: 16,
-  },
-
-  botao: {
-    marginTop: 25,
-  },
-
-  resultado: {
-    marginTop: 20,
-    fontSize: 20,
-  },
-});
+} 
